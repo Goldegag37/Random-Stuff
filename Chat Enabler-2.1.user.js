6f6486a1be5b7a1b7ac630bdcd5593c6ca1b5ac8
Goldegag37 <130252244+Goldegag37@users.noreply.github.com>
2024-09-02 21:55:52 -0700

    Add files via upload
    for sharing, of course.

---
+++ Chat Enabler-2.1.user.js
@@ -0,0 +1,61 @@
+// ==UserScript==
+// @name         Chat Enabler
+// @namespace    http://tampermonkey.net/
+// @version      2.1
+// @description  re-enables chat on a world that had it disabled by the owner.
+// @author       yagton
+// @match        https://github.com/slaskax/owotscripts/blob/master/scripts/chatenabler.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=ourworldoftext.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    /* yagton's "Chat Enabler" script (version 2.1)
+ * This script re-enables chat on a world that had it disabled by the owner.
+ *
+ * This is free and unencumbered software released into the public domain.
+ * For more information, please refer to <http://unlicense.org/> */
+
+(() => {
+    if (Permissions.can_chat(state.userModel, state.worldModel)) return;
+
+    let w_url = `ws${window.location.protocol == "https:" ? "s" : ""}://${
+        window.location.host
+    }/w${state.worldModel.pathname}/ws/${window.location.search || ""}`;
+    let sock = new WebSocket(w_url);
+
+    sock.onopen = () => {
+        window.selectedChatTab = 0;
+        ws_functions.propUpdate({props: [{type: "chat", value: 0}]});
+        w.showChat();
+        elm.chat_open.style.backgroundColor = "#990000";
+        elm.total_unread.style.color = "#FFFF00";
+
+        sock.onmessage = msg => {
+            let data = JSON.parse(msg.data);
+            if (data.kind.startsWith("chat")
+               || ["channel", "user_count"].includes(data.kind)) {
+                ws_functions[data.kind](data);
+            }
+        };
+
+        sock.send(JSON.stringify({ kind: "chathistory" }));
+        w.on("chat", e => {
+            w.emit("chatmod", e);
+            if (e.hide) return;
+            event_on_chat(e);
+        });
+
+        network.chat = (message, location, nickname, color) => {
+            sock.send(JSON.stringify({
+                kind: "chat",
+                nickname,
+                message,
+                location,
+                color,
+            }));
+        };
+    };
+})();
+})();
